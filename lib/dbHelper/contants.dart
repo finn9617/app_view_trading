f50a03aafc0acc287027ac38a455284a18f6e054
Finn <nguyenlaai10@gmail.com>
2024-02-21 09:46:17 +0700

    updatte

--- lib/dbHelper/contants.dart
+++ lib/dbHelper/contants.dart
@@ -1,4 +1,4 @@
 const mongoConnUrl =
     'mongodb+srv://nguyenlaai14:DbHiAde1TfHcZStb@app-clone.zmlbxlk.mongodb.net/appClone?retryWrites=true&w=majority';
 const userColletionDB = "app_clone";
-const idApp = "65d1c240915d30ab3eb5c0c0";
+const idApp = "12331231";
